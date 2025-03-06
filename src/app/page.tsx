1bc834db1fcf658d7ddb6e15b4fb210759042c4c
Hifnie Nazih <hifninazih111@gmail.com>
2025-03-06 19:17:05 +0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad at nisi
+        necessitatibus tempora nihil? Maxime tempora mollitia quas id
+        voluptatibus, commodi, error non incidunt praesentium nesciunt optio,
+        voluptatem ab quasi!
+      </p>
     </>
   );
 }
